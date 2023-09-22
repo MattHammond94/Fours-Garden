439d7e7229ad542e9869f9c59e221ba819de34db
Matt Hammond <mjhammond94@gmail.com>
2023-09-22 15:28:03 +0100

    Resolve Navbar bug

--- fours-garden/src/components/NavBar.jsx
+++ fours-garden/src/components/NavBar.jsx
@@ -6,15 +6,18 @@ const NavBar = () => {
 
   const toggleNav = () => {
     const currentState = isOpen
-    console.log(currentState);
     return currentState ? setIsOpen(false) : setIsOpen(true)
+  }
+
+  const togglerStyle = {
+    left: isOpen ? '260px' : '5px'
   }
 
   return (
     <div className={`navbar ${isOpen ? 'open' : ''}`}>
-      <button className="navbar-toggler" onClick={ toggleNav }>Open Nav</button>
-      <nav className="navbar">
-        <img src="../../public/4-circle.png" alt="logo" />
+      <button className="navbar-toggler" style={togglerStyle} onClick={ toggleNav }>&#9776;</button>
+      <nav className="navbar-child">
+        <img src="/4-circle.png" alt="logo" />
         <NavLink to="/artwork/drawings">Drawings</NavLink>
         <NavLink to="/artwork/paintings">Paintings</NavLink>
         <NavLink to="/artwork/prints">Prints</NavLink>
