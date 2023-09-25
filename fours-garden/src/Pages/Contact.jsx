7892428cc679efbd39184951738204380e93e2e7
Matt Hammond <mjhammond94@gmail.com>
2023-09-25 15:28:10 +0100

    Install emailjs - update contact form component - add minor styling changes

--- fours-garden/src/Pages/Contact.jsx
+++ fours-garden/src/Pages/Contact.jsx
@@ -1,6 +1,7 @@
-import ContactForm from "../components/ContactForm"
+import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
+
   return (
     <>
       <div>
@@ -9,8 +10,8 @@ const Contact = () => {
         <img src="" alt="" />
       </div>
       <div>
-        <h1>Contact</h1>
-        <p>You can find me on instagram <a href="https://www.instagram.com/four_kid/">@four_kid</a>, but alternatively if you have a query or want to get in touch please complete the form below:</p>
+        <h1 className="contact-header">Contact</h1>
+        <p className="contact-text">You can find and get in touch with me on instagram <a href="https://www.instagram.com/four_kid/">@four_kid.</a><br />Alternatively if you have a query or want to get in touch please complete the form below:</p>
       </div>
       <ContactForm />
     </>
--- fours-garden/src/components/ContactForm.jsx
+++ fours-garden/src/components/ContactForm.jsx
@@ -1,16 +1,29 @@
-import { useState } from "react"
+import { useState, useRef } from "react";
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
+  const contactForm = useRef();
+
+  const contactFormSubmission = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(import.meta.env.VITE_EMAIL_SERVICE_ID, import.meta.env.VITE_EMAIL_TEMPLATE_ID, contactForm.current, import.meta.env.VITE_EMAIL_PUBLIC_KEY)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
 
   return (
     <div className="contact-form">
-      <form action="" method="POST">
+      <form ref={contactForm} onSubmit={contactFormSubmission}>
         <label>Name:</label>
-        <input type="text" placeholder="Full Name"/>
+        <input type="text" name="user_name" placeholder="Full Name"/>
         <label>Email Address:</label>
-        <input type="email" placeholder="Email Address"/>
+        <input type="email" name="user_email_address" placeholder="Email Address"/>
         <label>Query:</label>
-        <textarea name="Query" placeholder="Query"></textarea>
+        <textarea name="query" placeholder="Query" />
         <button className="contact-submit-btn" type="submit">Send</button>
       </form>
     </div>
