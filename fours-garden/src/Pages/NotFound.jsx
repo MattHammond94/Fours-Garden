bdd1baebfe0f80569a91cd57e89300f18307d50c
Matt Hammond <mjhammond94@gmail.com>
2023-09-25 10:58:15 +0100

    Add CSS for notfound page comp

--- fours-garden/src/Pages/NotFound.jsx
+++ fours-garden/src/Pages/NotFound.jsx
@@ -1,11 +1,15 @@
 
 const NotFound = () => {
   return (
-    <div className="404-container">
-      <img src="../../title-icon.png" alt="logo" />
-      <h1>404</h1>
-      <p>Nothing exists here. This is likely because the mighty and powerful Four has not designed this yet. To return to the homepage press <span>here</span> Yours truly, Four Kid</p>
-      <img src="../../public/four-signature.png" alt="sig" />
+    <div className="not-found-container">
+      <div className="not-found-img-container">
+        <img className="four-logo" src="../../title-icon.png" alt="logo" />
+      </div>
+      <div className="not-found-content-container">
+        <h1>404</h1>
+        <p>Nothing exists here. This is likely because the mighty and powerful Four has not designed this yet. To return to the homepage press <a href="/">here.</a> <br />Yours truly, <br />Four Kid</p>
+        <img className="four-signature" src="../../public/four-signature.png" alt="sig" />
+      </div>
     </div>
   )
 }
