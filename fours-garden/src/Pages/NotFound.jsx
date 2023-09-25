7d3bde4f5100f309e87dff3f0a6547a07a134c56
Matt Hammond <mjhammond94@gmail.com>
2023-09-25 09:38:44 +0100

    Add elements to 404 page comp

--- fours-garden/src/Pages/NotFound.jsx
+++ fours-garden/src/Pages/NotFound.jsx
@@ -1,7 +1,12 @@
 
 const NotFound = () => {
   return (
-    <h1>Not Found</h1>
+    <div className="404-container">
+      <img src="../../title-icon.png" alt="logo" />
+      <h1>404</h1>
+      <p>Nothing exists here. This is likely because the mighty and powerful Four has not designed this yet. To return to the homepage press <span>here</span> Yours truly, Four Kid</p>
+      <img src="../../public/four-signature.png" alt="sig" />
+    </div>
   )
 }
 
