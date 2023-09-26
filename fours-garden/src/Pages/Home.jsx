00238967321329d6b0b9431037133c385b1b49ed
Matt Hammond <mjhammond94@gmail.com>
2023-09-27 00:03:49 +0100

    Add img files - Update slider element - update 404 page styling

--- fours-garden/src/Pages/Home.jsx
+++ fours-garden/src/Pages/Home.jsx
@@ -3,9 +3,11 @@ import Slider from '../components/Slider';
 const Home = () => {
   return (
     <div className="homepage-container">
-      <img src="../../public/home-page-title.png" />
-      <h1>Welcome to <span>Fours</span> Garden of Wonders</h1>
-      <p>An artist based on the south coast on the UK. If you're looking for to collaborate or need some work done please reach out through the contacts page <a href="/contact">here.</a></p>
+      <div className="homepage-content">
+        <img src="../../public/home-page-title.png" />
+        <h1>Welcome to <span>Fours</span> Garden of Wonders</h1>
+        <p>An artist based on the south coast on the UK. If youre looking for to collaborate or need some work done please reach out through the contacts page <a href="/contact">here.</a></p>
+      </div>
       <Slider />
     </div>
   )
--- fours-garden/src/components/Slider.jsx
+++ fours-garden/src/components/Slider.jsx
@@ -3,13 +3,27 @@ const Slider = () => {
   return (
     <section className="slide-section">
     <div className="slider">
-      <div className="box1"></div>
-      <div className="box2"></div>
-      <div className="box3"></div>
-      <div className="box4"></div>
-      <div className="box5"></div>
-      <div className="box6"></div>
-      <div className="box7"></div>
+      <div className="box1">
+        <img src="1-Swatch.jpg" />
+      </div>
+      <div className="box2">
+        <img src="2.jpg" />
+      </div>
+      <div className="box3">
+        <img src="3.jpg" />
+      </div>
+      <div className="box4">
+        <img src="4.JPG" />
+      </div>
+      <div className="box5">
+        <img src="5.png" />
+      </div>
+      <div className="box6">
+        <img src="6.JPG"/>
+      </div>
+      <div className="box7">
+        <img src="7.JPG" />
+      </div>
     </div>
   </section>
   )
