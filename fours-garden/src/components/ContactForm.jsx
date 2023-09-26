3d382901292c2b3d78b324352e024844ec116a0a
Matt Hammond <mjhammond94@gmail.com>
2023-09-26 16:55:09 +0100

    Add validation for each field - refactor contactform component

--- fours-garden/src/components/ContactForm.jsx
+++ fours-garden/src/components/ContactForm.jsx
@@ -3,71 +3,95 @@ import emailjs from '@emailjs/browser';
 import { toast } from 'react-toastify';
 
 const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [query, setQuery] = useState('');
-  const [errorMessage, setErrorMessage] = useState(null);
-
+  const initialState = { name: '', email: '', query: '' }
+  const [formValues, setFormValues] = useState(initialState);
+  const [errorMessages, setErrorMessages] = useState({});
   const contactForm = useRef();
 
-  const nameValidator = () => {
-    
-    name.trim()
-
-    if (name.length <= 0) {
-      setErrorMessage('This field cannot be left empty.')
-      return false;
-    }
-
-    if (!/^[A-Za-z]+ [A-Za-z]+$/.test(name)) {
-      setErrorMessage('Name should appear as first name and last name only')
-      return false;
-    }
-
-    if (name.length > 20) {
-      setErrorMessage('Name cannot be longer than 20 characters.')
-      return false;
-    }
-
-    return true;
+  const handleChange = (e) => {
+    const { name, value  } = e.target
+    setFormValues({ ...formValues, [name]: value });
   }
 
-  const contactFormSubmission = (e) => {
-    e.preventDefault();
+  const validation = (values) => {
+    const errors = {}
+    const acceptedEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const acceptedName = /^[A-Za-z]+ [A-Za-z]+$/
 
-
-    if (nameValidator()) {
-      emailjs.sendForm(import.meta.env.VITE_EMAIL_SERVICE_ID, 
-        import.meta.env.VITE_EMAIL_TEMPLATE_ID, 
-        contactForm.current, 
-        import.meta.env.VITE_EMAIL_PUBLIC_KEY)
-        .then((result) => {
-            console.log(result.text);
-            setName('');
-            setEmail('');
-            setQuery('');
-            toast('Your query was succesfully sent!', {
-              className: 'contact-toast',
-              progressClassName: 'contact-toast-progress'
-            });
-        }, (error) => {
-            toast.error('Query unsuccessful')
-            console.log(error.text);
-        });
+    if (values.name.length <= 0) {
+      errors.name = "Please complete this field to continue."
+    } 
+    
+    if (!acceptedName.test(values.name)) {
+      errors.name = "Name must appear in first name, last name format only."
+    } 
+    
+    if (values.name.length > 20) {
+      errors.name = "Name must be less than 20 characters."
     }
 
+    if (values.email.length <= 0) {
+      errors.email = "Please complete this field to continue."
+    }
+
+    if (!acceptedEmail.test(values.email)) {
+      errors.email = "Please input a valid email address."
+    }
+
+    if (values.query.length <= 0) {
+      errors.query = "Please complete this field to continue."
+    }
+
+    if (values.query.length > 1000) {
+      errors.query = "Query cannot exceed 1000 characters."
+    }
+
+    return errors;
+  }
+
+  const contactFormSubmission = async (e) => {
+    e.preventDefault();
+
+    const errors = validation(formValues);
+
+    setErrorMessages(errors);
+
+    if (Object.keys(errors).length === 0) {
+
+      setFormValues(initialState);
+
+      try {
+        await emailjs.sendForm(
+          import.meta.env.VITE_EMAIL_SERVICE_ID, 
+          import.meta.env.VITE_EMAIL_TEMPLATE_ID, 
+          contactForm.current, 
+          import.meta.env.VITE_EMAIL_PUBLIC_KEY
+        );
+      
+        toast('Your query was succesfully sent!', {
+          className: 'contact-toast',
+          progressClassName: 'contact-toast-progress'
+        });
+
+      } catch(error) {
+        toast.error('Your query was unsuccesful - please try again')
+        console.error(error)
+      }
+    }
   }
 
   return (
     <div className="contact-form">
       <form ref={contactForm} onSubmit={contactFormSubmission}>
         <label>Name:</label>
-        <input type="text" name="user_name" placeholder="Full Name" value={ name } onChange={ (e) => setName(e.target.value) }/>
-        { errorMessage && <p>{ errorMessage }</p> }
+        <input type="text" name="name" placeholder="Full Name" value={ formValues.name } onChange={ handleChange }/>
+        <p>{ errorMessages.name }</p>
         <label>Email Address:</label>
-        <input type="email" name="user_email_address" placeholder="Email Address" value={ email } onChange={ (e) => setEmail(e.target.value) }/>
+        <input type="text" name="email" placeholder="Email Address" value={ formValues.email } onChange={ handleChange }/>
+        <p>{ errorMessages.email }</p>
         <label>Query:</label>
-        <textarea name="query" placeholder="Query" value={ query } onChange={ (e) => setQuery(e.target.value) } />
+        <input type="text" name="query" placeholder="Your Query" value={ formValues.query } onChange={ handleChange } />
+        <p>{ errorMessages.query }</p>
         <button className="contact-submit-btn" type="submit">Send</button>
       </form>
     </div>
