65a914aa94d240e911cd1a4ecc9e85b4fe3d36cb
Matt Hammond <mjhammond94@gmail.com>
2023-09-27 10:26:01 +0100

    Complete styling for landing page - Update home page comp - modify home styling

--- fours-garden/src/Pages/Home.jsx
+++ fours-garden/src/Pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="homepage-container">
       <div className="homepage-content">
-        <img src="../../public/home-page-title.png" />
+        <img src="home-page-title.png" />
         <h1>Welcome to <span>Fours</span> Garden of Wonders</h1>
         <p>An artist based on the south coast on the UK. If youre looking for to collaborate or need some work done please reach out through the contacts page <a href="/contact">here.</a></p>
       </div>
--- fours-garden/src/Pages/LandingPage.jsx
+++ fours-garden/src/Pages/LandingPage.jsx
@@ -2,7 +2,7 @@
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <h1 className="title">Four Kid</h1>
+      <h1 className="title">_Four Kid_</h1>
       <h3 className="statement">Artist <span>|</span> Illustrator <span>|</span> Animator</h3>
       <img src="/landing-page-circle.png" alt="circle" />
       <a href="/the-garden">Enter</a>
