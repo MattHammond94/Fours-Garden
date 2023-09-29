5648ecaa210e332fcca207861fbb8b4e1a279974
Matt Hammond <mjhammond94@gmail.com>
2023-09-29 12:25:38 +0100

    Update artworknav - update index CSS

--- fours-garden/src/components/ArtworkNavBar.jsx
+++ fours-garden/src/components/ArtworkNavBar.jsx
@@ -1,14 +1,29 @@
 
 const ArtworkNavBar = ({ variant }) => {
 
+  const homeButton = (
+    <a href="/the-garden">Home</a>
+  )
+
   if (variant === 'photographs') {
     
     return (
-      <div className="artwork-nav-container">
+      <div className="artwork-nav-container" style={{backgroundImage: "url(" + '/navbar3.jpg' + ")"}}>
         <div className="content">
           <h1>Photographs</h1>
         </div>
-        <a href="/the-garden">Home</a>
+        {homeButton}
+      </div>
+    )
+  }
+
+  if (variant === 'drawings') {
+    return (
+      <div className="artwork-nav-container" style={{backgroundImage: "url(" + '/navbar1.jpg' + ")"}}>
+        <div className="content">
+          <h1>Drawings</h1>
+        </div>
+        {homeButton}
       </div>
     )
   }
