984e917e32c0d1aa1f8ca36e0e80b9b4fed65f83
Matt Hammond <mjhammond94@gmail.com>
2023-10-16 16:26:51 +0100

    Further adjustments to navbar

--- fours-garden/src/components/NavBar.jsx
+++ fours-garden/src/components/NavBar.jsx
@@ -14,14 +14,14 @@ const NavBar = ({ variant }) => {
   if (variant === "light") {
     togglerStyle = {
       left: isOpen ? '240px' : '12px',
-      top: '70px',
+      top: '67px',
       color: 'white'
     }
   }
 
   return (
     <div className={`navbar ${isOpen ? 'open' : ''}`}>
-      { isOpen ? <button className="navbar-toggler" style={togglerStyle} onClick={ toggleNav }>&#10094;</button> : <button className="navbar-toggler" style={togglerStyle} onClick={ toggleNav }>&#10095;</button> }
+      <button className="navbar-toggler" style={togglerStyle} onClick={ toggleNav }>&#9776;</button>
       <nav className="navbar-child">
         <img src="/title-icon.png" alt="logo" />
         <NavLink to="/the-garden/drawings">Drawings</NavLink>
