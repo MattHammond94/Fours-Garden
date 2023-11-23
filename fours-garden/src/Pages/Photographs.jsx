a37044520d33ed36233341664d8a4302c0a17a25
Matt Hammond <mjhammond94@gmail.com>
2023-11-23 23:23:49 +0000

    Reoptimize all images in photo gallery

--- fours-garden/src/Pages/Photographs.jsx
+++ fours-garden/src/Pages/Photographs.jsx
@@ -9,22 +9,26 @@ const Photographs = () => {
   const [imageLoaded, setImageLoaded] = useState(false);
 
   const streetSlides = [
-    { url: '/Street/1-resized.jpg', lowResUrl: '/Street/1-blur.jpg' },
-    { url: '/Street/2-resized.jpg', lowResUrl: '/Street/2-blur.jpg' },
-    { url: '/Street/3-resized.jpg', lowResUrl: '/Street/3-blur.jpg' },
-    { url: '/Street/4-resized.jpg', lowResUrl: '/Street/4-blur.jpg' },
-    { url: '/Street/5-resized.jpg', lowResUrl: '/Street/5-blur.jpg' },
-    { url: '/Street/6-resized.jpg', lowResUrl: '/Street/6-blur.jpg' },
-    { url: '/Street/7-resized.jpg', lowResUrl: '/Street/7-blur.jpg' },
-    { url: '/Street/8-resized.jpg', lowResUrl: '/Street/8-blur.jpg' },
-    { url: '/Street/9-resized.jpg', lowResUrl: '/Street/9-blur.jpg' },
-    { url: '/Street/10-resized.jpg', lowResUrl: '/Street/10-blur.jpg' },
-    { url: '/Street/11-resized.jpg', lowResUrl: '/Street/11-blur.jpg' },
-    { url: '/Street/12-resized.jpg', lowResUrl: '/Street/12-blur.jpg' },
-    { url: '/Street/13-resized.jpg', lowResUrl: '/Street/13-blur.jpg' },
-    { url: '/Street/14-resized.jpg', lowResUrl: '/Street/14-blur.jpg' },
-    { url: '/Street/15-resized.jpg', lowResUrl: '/Street/15-blur.jpg' },
-    { url: '/Street/16-resized.jpg', lowResUrl: '/Street/16-blur.jpg' }
+    { url: '/Street/1.jpg', lowResUrl: '/Street/1-blur.jpg' },
+    { url: '/Street/2.jpg', lowResUrl: '/Street/2-blur.jpg' },
+    { url: '/Street/3.jpg', lowResUrl: '/Street/3-blur.jpg' },
+    { url: '/Street/4.jpg', lowResUrl: '/Street/4-blur.jpg' },
+    { url: '/Street/5.jpg', lowResUrl: '/Street/5-blur.jpg' },
+    { url: '/Street/6.jpg', lowResUrl: '/Street/6-blur.jpg' },
+    { url: '/Street/7.jpg', lowResUrl: '/Street/7-blur.jpg' },
+    { url: '/Street/8.jpg', lowResUrl: '/Street/8-blur.jpg' },
+    { url: '/Street/9.jpg', lowResUrl: '/Street/9-blur.jpg' },
+    { url: '/Street/10.jpg', lowResUrl: '/Street/10-blur.jpg' },
+    { url: '/Street/11.jpg', lowResUrl: '/Street/11-blur.jpg' },
+    { url: '/Street/12.jpg', lowResUrl: '/Street/12-blur.jpg' },
+    { url: '/Street/13.jpg', lowResUrl: '/Street/13-blur.jpg' },
+    { url: '/Street/14.jpg', lowResUrl: '/Street/14-blur.jpg' },
+    { url: '/Street/15.jpg', lowResUrl: '/Street/15-blur.jpg' },
+    { url: '/Street/16.jpg', lowResUrl: '/Street/16-blur.jpg' },
+    { url: '/Street/17.jpg', lowResUrl: '/Street/16-blur.jpg' },
+    { url: '/Street/18.jpg', lowResUrl: '/Street/16-blur.jpg' },
+    { url: '/Street/19.jpg', lowResUrl: '/Street/16-blur.jpg' },
+    { url: '/Street/20.jpg', lowResUrl: '/Street/16-blur.jpg' }
   ]
 
   const landscapesSlides = [
@@ -39,26 +43,28 @@ const Photographs = () => {
     { url: '/Landscapes/9.jpg', lowResUrl: '/Landscapes/9-blur.jpg' },
     { url: '/Landscapes/10.jpg', lowResUrl: '/Landscapes/10-blur.jpg' },
     { url: '/Landscapes/11.jpg', lowResUrl: '/Landscapes/11-blur.jpg' },
-    { url: '/Landscapes/12.jpg', lowResUrl: '/Landscapes/12-blur.jpg' }
+    { url: '/Landscapes/12.jpg', lowResUrl: '/Landscapes/12-blur.jpg' },
+    { url: '/Landscapes/13.jpg', lowResUrl: '/Landscapes/13-blur.jpg' },
+    { url: '/Landscapes/14.jpg', lowResUrl: '/Landscapes/14-blur.jpg' },
+    { url: '/Landscapes/15.jpg', lowResUrl: '/Landscapes/15-blur.jpg' },
+    { url: '/Landscapes/16.jpg', lowResUrl: '/Landscapes/16-blur.jpg' }
   ]
 
   const miscSlides = [
-    { url: '/Misc/1.jpg', lowResUrl: '/Misc/1-blur.jpg' },
-    { url: '/Misc/2.jpg', lowResUrl: '/Misc/2-blur.jpg' },
-    { url: '/Misc/3.jpg', lowResUrl: '/Misc/3-blur.jpg' },
-    { url: '/Misc/4.jpg', lowResUrl: '/Misc/4-blur.jpg' },
-    { url: '/Misc/5.jpg', lowResUrl: '/Misc/5-blur.jpg' },
-    { url: '/Misc/6.jpg', lowResUrl: '/Misc/6-blur.jpg' },
-    { url: '/Misc/7.jpg', lowResUrl: '/Misc/7-blur.jpg' },
-    { url: '/Misc/8.jpg', lowResUrl: '/Misc/8-blur.jpg' },
-    { url: '/Misc/9.jpg', lowResUrl: '/Misc/9-blur.jpg' },
-    { url: '/Misc/10.jpg', lowResUrl: '/Misc/10-blur.jpg' },
-    { url: '/Misc/11.jpg', lowResUrl: '/Misc/11-blur.jpg' },
-    { url: '/Misc/12.jpg', lowResUrl: '/Misc/12-blur.jpg' },
-    { url: '/Misc/13.jpg', lowResUrl: '/Misc/13-blur.jpg' },
-    { url: '/Misc/14.jpg', lowResUrl: '/Misc/14-blur.jpg' },
-    { url: '/Misc/15.jpg', lowResUrl: '/Misc/15-blur.jpg' },
-    { url: '/Misc/16.jpg', lowResUrl: '/Misc/16-blur.jpg' }
+    { url: '/Misc/1-resized.jpg', lowResUrl: '/Misc/1-blur.jpg' },
+    { url: '/Misc/2-resized.jpg', lowResUrl: '/Misc/2-blur.jpg' },
+    { url: '/Misc/3-resized.jpg', lowResUrl: '/Misc/3-blur.jpg' },
+    { url: '/Misc/4-resized.jpg', lowResUrl: '/Misc/4-blur.jpg' },
+    { url: '/Misc/5-resized.jpg', lowResUrl: '/Misc/5-blur.jpg' },
+    { url: '/Misc/6-resized.jpg', lowResUrl: '/Misc/6-blur.jpg' },
+    { url: '/Misc/7-resized.jpg', lowResUrl: '/Misc/7-blur.jpg' },
+    { url: '/Misc/8-resized.jpg', lowResUrl: '/Misc/8-blur.jpg' },
+    { url: '/Misc/9-resized.jpg', lowResUrl: '/Misc/9-blur.jpg' },
+    { url: '/Misc/10-resized.jpg', lowResUrl: '/Misc/10-blur.jpg' },
+    { url: '/Misc/11-resized.jpg', lowResUrl: '/Misc/11-blur.jpg' },
+    { url: '/Misc/12-resized.jpg', lowResUrl: '/Misc/12-blur.jpg' },
+    { url: '/Misc/13-resized.jpg', lowResUrl: '/Misc/13-blur.jpg' },
+    { url: '/Misc/14-resized.jpg', lowResUrl: '/Misc/14-blur.jpg' }
   ]
 
   const openModalWithSlides = (slides) => {
